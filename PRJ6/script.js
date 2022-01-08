b4c4c8c86b1216b29b0b4d89c2bacec1a2cc8ee0
thanh.liem <liem.pv.950@aptechlearning.edu.vn>
2022-01-08 20:49:09 +0700

    PRJ6

---
+++ PRJ6/script.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes() {
+    const triggerBottom = (window.innerHeight / 5) * 4;
+    console.log(triggerBottom)
+    boxes.forEach((box, index) => {
+        const boxTop = box.getBoundingClientRect().top;
+        console.log(boxTop, index)
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
