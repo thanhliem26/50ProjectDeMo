9b73add82436c0187d5bb1beb09df36029cf1acc
thanh.liem <liem.pv.950@aptechlearning.edu.vn>
2022-01-11 22:48:44 +0700

    PRJ8

---
+++ PRJ8/script.js
@@ -0,0 +1,11 @@
+const labels = document.querySelectorAll('.form-controll label');
+
+labels.forEach((label) => {
+    label.innerHTML = label.innerText
+      .split('')
+      .map(
+        (letter, idx) =>
+          `<span style="transition-delay: ${idx * 30}ms">${letter}</span>`,
+      )
+      .join('');
+  });
